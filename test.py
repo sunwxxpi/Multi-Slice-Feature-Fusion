9c3deb77f1b1fd2721823fefeec20a4ac8f4c7c2
Sunwoo Pi <ppssww1202@naver.com>
2025-01-10 07:12:54 +0000

    Update argument parsing in train.py and test.py: streamline paths, adjust defaults, and remove redundant dataset configuration

--- test.py
+++ test.py
@@ -8,22 +8,23 @@ import torch
 import torch.backends.cudnn as cudnn
 import segmentation_models_pytorch as smp
 from glob import glob
-from datasets.dataset import COCA_dataset
 from tester import inference
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--volume_path', type=str, default='./data/COCA/test_vol_h5', help='root dir for validation volume data')
-parser.add_argument('--dataset', type=str, default='COCA', help='experiment_name')
-parser.add_argument('--list_dir', type=str, default='./data/COCA/lists_COCA', help='list dir')
+parser.add_argument('--dataset', type=str, default='COCA', help='dataset name')
+parser.add_argument('--volume_path', type=str, default='/home/psw/AVS-Diagnosis/COCA_1frame/test_vol_h5', help='root dir for validation volume data')
+parser.add_argument('--list_dir', type=str, default='/home/psw/AVS-Diagnosis/COCA_1frame/lists_COCA', help='list dir')
 parser.add_argument('--num_classes', type=int, default=5, help='output channel of network')
-parser.add_argument('--max_epochs', type=int, default=1000, help='maximum epoch number to train')
-parser.add_argument('--batch_size', type=int, default=96, help='batch_size per gpu')
-parser.add_argument('--img_size', type=int, default=224, help='input patch size of network input')
-parser.add_argument('--test_save_dir', type=str, default='./predictions', help='saving prediction as nii!')
+parser.add_argument('--max_epochs', type=int, default=300, help='maximum epoch number to train')
+parser.add_argument('--batch_size', type=int, default=16, help='batch_size per gpu')
+parser.add_argument('--base_lr', type=float, default=0.00001, help='segmentation network learning rate')
+parser.add_argument('--img_size', type=int, default=512, help='input patch size of network input')
+parser.add_argument('--encoder', type=str, default='resnet50', help='for segmentation_models_pytorch encoder')
+parser.add_argument('--exp_setting', type=str,  default='default', help='description of experiment setting')
 parser.add_argument('--deterministic', type=int, default=1, help='whether use deterministic training')
-parser.add_argument('--base_lr', type=float, default=0.01, help='segmentation network learning rate')
-parser.add_argument('--seed', type=int, default=1234, help='random seed')
+parser.add_argument('--seed', type=int, default=42, help='random seed')
 parser.add_argument('--is_savenii', action="store_true", help='whether to save results during inference')
+parser.add_argument('--z_spacing', type=int, default=3, help='z spacing of the volume')
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -39,34 +40,6 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
 
-    dataset_name = args.dataset
-    dataset_config = {
-        'COCA': {
-            'Dataset': COCA_dataset,
-            'volume_path': '/home/psw/AVS-Diagnosis/COCA_1frame/test_vol_h5',
-            'list_dir': '/home/psw/AVS-Diagnosis/COCA_1frame/lists_COCA',
-            'num_classes': 5,
-            'max_epochs': 300,
-            'batch_size': 16,
-            'base_lr': 0.00001,
-            'img_size': 512,
-            'encoder': 'resnet50',
-            'exp_setting': 'default',
-            'z_spacing': 3,
-        },
-    }
-    
-    args.volume_path = dataset_config[dataset_name]['volume_path']
-    args.list_dir = dataset_config[dataset_name]['list_dir']
-    args.num_classes = dataset_config[dataset_name]['num_classes']
-    args.max_epochs = dataset_config[dataset_name]['max_epochs']
-    args.batch_size = dataset_config[dataset_name]['batch_size']
-    args.base_lr = dataset_config[dataset_name]['base_lr']
-    args.img_size = dataset_config[dataset_name]['img_size']
-    args.encoder = dataset_config[dataset_name]['encoder']
-    args.exp_setting = dataset_config[dataset_name]['exp_setting']
-    args.z_spacing = dataset_config[dataset_name]['z_spacing']
-    
     net = smp.Unet(
             encoder_name=args.encoder,
             encoder_weights="imagenet",
@@ -74,11 +47,11 @@ if __name__ == "__main__":
             classes=args.num_classes,
             ).cuda()
     
-    exp_path = os.path.join(net.__class__.__name__ + '_' + args.encoder, dataset_name + '_' + str(args.img_size), args.exp_setting)
+    exp_path = os.path.join(net.__class__.__name__ + '_' + args.encoder, args.dataset + '_' + str(args.img_size), args.exp_setting)
     parameter_path = 'epo' + str(args.max_epochs) + '_bs' + str(args.batch_size) + '_lr' + str(args.base_lr)
     
     snapshot_path = os.path.join("./model/", exp_path, parameter_path)
-    best_model_path = glob(os.path.join(snapshot_path, '*_best_model.pth'))[0] # 유일한 best_model 파일 선택
+    best_model_path = glob(os.path.join(snapshot_path, '*_best_model.pth'))[0]
     if not best_model_path:
         raise FileNotFoundError(f"Best model not found at {snapshot_path}")
     net.load_state_dict(torch.load(best_model_path))
