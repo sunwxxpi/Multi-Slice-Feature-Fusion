8fa0b0f62198bee336ba22f759bee366a9f27b99
Sunwoo Pi <ppssww1202@naver.com>
2025-03-09 11:37:50 +0000

    Update root_path and list_dir arguments for consistency in dataset directory structure

--- test.py
+++ test.py
@@ -12,8 +12,8 @@ from tester import inference, get_attn_hook
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='COCA', help='dataset name')
-parser.add_argument('--root_path', type=str, default='/home/psw/AVS-Diagnosis/COCA_3frames/test_npz', help='root dir for validation volume data')
-parser.add_argument('--list_dir', type=str, default='/home/psw/AVS-Diagnosis/COCA_3frames/lists_COCA', help='list dir')
+parser.add_argument('--root_path', type=str, default='/home/psw/AVS-Diagnosis/COCA/COCA_3frames/test_npz', help='root dir for validation volume data')
+parser.add_argument('--list_dir', type=str, default='/home/psw/AVS-Diagnosis/COCA/COCA_3frames/lists_COCA', help='list dir')
 parser.add_argument('--num_classes', type=int, default=5, help='output channel of network')
 parser.add_argument('--max_epochs', type=int, default=300, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int, default=16, help='batch_size per gpu')
